071bb25a495e0c0450513a3b54ca7517d51469d3
trana <trana@PC-2227774.cnesnet.ad.cnes.fr>
2023-01-16 13:11:35 +0100

    sizeof N

--- container_docker/container/ldasoft/globalfit/src/GlobalFit.c
+++ container_docker/container/ldasoft/globalfit/src/GlobalFit.c
@@ -230,11 +230,15 @@ static void share_gbmcmc_model(struct GBMCMCData *gbmcmc_data,
     /* broadcast gbmcmc model to all non-UCB worker nodes */
     if(procID==root)
     {
+        printf("sizeof of N: %d\n", gf->tdi_ucb->N);
+
         printf("sizeof of TDI A: %d\n", (int) sizeof(gf->tdi_ucb->A));
+        printf("sizeof of TDI A * N: %d\n", (int) (sizeof(gf->tdi_ucb->A) * gf->tdi_ucb->N ) );
         printf("length of TDI A: %d\n", (int) ( sizeof(gf->tdi_ucb->A) / sizeof(gf->tdi_ucb->A[0]) ) );
         printf("sizeof of double type: %d\n", (int) ( sizeof(gf->tdi_ucb->A[0]) ) );
 
         printf("sizeof of TDI E: %d\n", (int) sizeof(gf->tdi_ucb->E));
+        printf("sizeof of TDI E * N: %d\n", (int) (sizeof(gf->tdi_ucb->E) * gf->tdi_ucb->N ) );
         printf("length of TDI E: %d\n", (int) ( sizeof(gf->tdi_ucb->E) / sizeof(gf->tdi_ucb->E[0]) ) );
         printf("sizeof of double type: %d\n", (int) ( sizeof(gf->tdi_ucb->E[0]) ) );
 
